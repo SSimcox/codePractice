6c6bbc752cea853fe656fb5474cdc43cb5fe68ec
Tj <tj.ferrell@gmail.com>
2016-10-07 10:04:41 -0600

    Initial commit (base hello world html file)

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+var gen = function(app){
+  /* GET home page. */
+
+  return router;
+};
+
+module.exports = function (app){
+  return gen(app);
+};
