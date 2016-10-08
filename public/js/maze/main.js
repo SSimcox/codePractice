5d12f00a25129e4d11b962950e7a2839a30f7259
Steven Simcox <steven.d.simcox@gmail.com>
2016-10-08 15:37:10 -0600

    Add language selector for editor, js to control it.

--- public/js/maze/main.js
+++ public/js/maze/main.js
@@ -84,6 +84,7 @@ $(document).ready(function(){
     console.log(h);
     //Set Editor height
     $("#editorContainer").attr("style", "height:" + (h - 50) + "px;");
+    $('#lang-select').on("change", setEditorLang);
 });
 
 //Editor Functions
@@ -96,7 +97,8 @@ editor.setOption({
 editor.resize();
 
 
-function setEditorLang(langNumber){
+function setEditorLang(){
+    var langNumber = Number($('#lang-select').val());
     var str = "ace/mode/c_cpp";
     switch(langNumber){
         case 7: str = "ace/mode/c_cpp";
