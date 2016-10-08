92d95728e82fc981b818e281ba5ccc043b60e52b
Steven Simcox <steven.d.simcox@gmail.com>
2016-10-08 13:47:50 -0600

    Estimated Maze Solution

--- game/stdIn.js
+++ game/stdIn.js
@@ -1,6 +1,6 @@
 module.exports = function (){
     var stdIn = {
-        'maze':"u\n d\n"
+        'maze':"d\nd\nd\nd\nd\nd\nd\nd\nd\nd\nd\nd\nl\nl\nl\nu\nu\nu\nu\nu\nu\nu\nu\nu\nu\nl\nl\nl\nd\nd\nd\nl\nl\nl\nl\nd\nd\nd\nd\nd\nr\nd\nd\nd\nl\nl\nl\nl\nu\nu\nu\nu\nu\nu\nl\nl\nl\nl\n"
     };
     return stdIn;
 };
